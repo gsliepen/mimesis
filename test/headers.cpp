c6f3f648842860c6f9cf0836f7c7fab440b6edc7
Guus Sliepen <guus@lightbts.info>
2017-09-10 22:37:05 +0200

    Improve code coverage of the headers test.

--- test/headers.cpp
+++ test/headers.cpp
@@ -8,9 +8,9 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 	Mimesis::Part part;
-	
+
 	assert(part.get_headers().empty());
-		
+
 	// Set using []
 	part["foo"] = "bar";
 	assert(part["foo"] == "bar");
@@ -59,6 +59,17 @@ int main(int argc, char *argv[]) {
 	assert(part.get_header_value("aap").empty());
 	assert(part.get_header_parameter("aap", "noot") == "mies");
 
+	// Change value but not parameters
+	part.set_header_value("baz", "quux2");
+	assert(part.get_header("baz") == "quux2; parameter=value2; foo=bar");
+
+	part.set_header_value("a", "b");
+	assert(part.get_header("a") == "b");
+
+	// Erase header
+	part.erase_header("a");
+	assert(part.get_header("a").empty());
+
 	// Appback header
 	part.append_header("insert", "back");
 	assert(part.get_header("insert") == "back");
@@ -74,12 +85,12 @@ int main(int argc, char *argv[]) {
 		assert(!header.first.empty());
 		assert(!header.second.empty());
 	}
-	assert(headers.front().second == "front");	
+	assert(headers.front().second == "front");
 	assert(headers[1].first == "foo");
 	assert(headers[2].first == "baz");
 	assert(headers[3].first == "aap");
 	assert(headers[3].second == "; noot=mies");
-	assert(headers.back().second == "back");	
+	assert(headers.back().second == "back");
 
 	// Delete single header
 	part.set_header("foo", {});
@@ -88,20 +99,27 @@ int main(int argc, char *argv[]) {
 	// Intermediate result
 	assert(part.to_string() ==
 			"insert: front\r\n"
-			"baz: quux; parameter=value2; foo=bar\r\n"
+			"baz: quux2; parameter=value2; foo=bar\r\n"
 			"aap: ; noot=mies\r\n"
 			"insert: back\r\n"
 			"\r\n");
 
-	// Erase header
+	// Erase multiple headers
 	part.erase_header("insert");
 	assert(part.get_header("insert").empty());
 
 	// Intermediate result
 	assert(part.to_string() ==
-			"baz: quux; parameter=value2; foo=bar\r\n"
+			"baz: quux2; parameter=value2; foo=bar\r\n"
 			"aap: ; noot=mies\r\n"
 			"\r\n");
+
+	// Const header access
+	const auto &cpart = part;
+	assert(cpart.get_headers().size() >= 2);
+	assert(cpart.get_header("baz") == "quux2; parameter=value2; foo=bar");
+	assert(cpart["aap"] == "; noot=mies");
+	assert(cpart["insert"].empty());
 
 	// Delete headers
 	part.clear_headers();
